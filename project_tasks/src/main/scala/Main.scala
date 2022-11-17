905009b1b0ca4a408a502f003a9d40416a9b65f9
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-11-17 11:43:34 +0100

    Co-authored-by: patryk1999 <patryk1999@users.noreply.github.com>

--- project_tasks/src/main/scala/Main.scala
+++ project_tasks/src/main/scala/Main.scala
@@ -1,12 +1,29 @@
-
 object Main extends App {
+  override def main(args: Array[String]) = {
+    var bank = new Bank
+    var transactionQueue = new TransactionQueue
+    transactionQueue.push(
+      new Transaction(
+        transactionQueue,
+        transactionQueue,
+        new Account(bank, 1000),
+        new Account(bank, 1000),
+        100,
+        1
+      )
+    )
+    println(transactionQueue.peek)
+    println(transactionQueue.isEmpty)
+    println(transactionQueue.pop)
+    println(transactionQueue.isEmpty)
+  }
 
-    def thread(body: => Unit): Thread = {
-        val t = new Thread {
-            override def run() = body
-        }
-        t.start
-        t
+  def thread(body: => Unit): Thread = {
+    val t = new Thread {
+      override def run() = body
     }
-  
+    t.start
+    t
+  }
+
 }
--- project_tasks/src/main/scala/Transaction.scala
+++ project_tasks/src/main/scala/Transaction.scala
@@ -1,5 +1,5 @@
 import exceptions._
-import scala.collection.mutable
+import scala.collection.immutable.Queue
 
 object TransactionStatus extends Enumeration {
   val SUCCESS, PENDING, FAILED = Value
@@ -7,53 +7,79 @@ object TransactionStatus extends Enumeration {
 
 class TransactionQueue {
 
-    // TODO
-    // project task 1.1
-    // Add datastructure to contain the transactions
+  // TODO
+  // project task 1.1
+  // Add datastructure to contain the transactions
+  private var queue: Queue[Transaction] = Queue()
 
-    // Remove and return the first element from the queue
-    def pop: Transaction = ???
+  private def mutateQueue(
+      function: Queue[Transaction] => Queue[Transaction]
+  ): Unit =
+    synchronized {
+      this.queue = function(queue)
+    }
 
-    // Return whether the queue is empty
-    def isEmpty: Boolean = ???
+  // Remove and return the first element from the queue
+  def pop: Transaction = {
+    val function: Queue[Transaction] => Queue[Transaction] =
+      (queue: Queue[Transaction]) => queue.dequeue._2
+    mutateQueue((queue: Queue[Transaction]) => {
+      queue.dequeue match {
+        case (head, tail) =>
+          tail
+      }
+      Queue[Transaction]()
+    })
+  }
+// def pq(q: Queue[List[Any]]): Unit = {
+//   q.dequeue match {
+//     case (head, tail) =>
+//       println(head + "..."); // ... process head element ...
+//       if (tail.nonEmpty) pq(tail)
+//   }
+// }
 
-    // Add new element to the back of the queue
-    def push(t: Transaction): Unit = ???
+  // Return whether the queue is empty
+  def isEmpty: Boolean = queue.isEmpty
 
-    // Return the first element from the queue without removing it
-    def peek: Transaction = ???
+  // Add new element to the back of the queue
+  def push(t: Transaction): Unit = mutateQueue(queue => queue.enqueue(t))
 
-    // Return an iterator to allow you to iterate over the queue
-    def iterator: Iterator[Transaction] = ???
+  // Return the first element from the queue without removing it
+  def peek: Transaction = queue.head
+
+  // Return an iterator to allow you to iterate over the queue
+  def iterator: Iterator[Transaction] = queue.iterator
 }
 
-class Transaction(val transactionsQueue: TransactionQueue,
-                  val processedTransactions: TransactionQueue,
-                  val from: Account,
-                  val to: Account,
-                  val amount: Double,
-                  val allowedAttemps: Int) extends Runnable {
+class Transaction(
+    val transactionsQueue: TransactionQueue,
+    val processedTransactions: TransactionQueue,
+    val from: Account,
+    val to: Account,
+    val amount: Double,
+    val allowedAttemps: Int
+) extends Runnable {
 
   var status: TransactionStatus.Value = TransactionStatus.PENDING
   var attempt = 0
 
   override def run: Unit = {
 
-      def doTransaction() = {
-          // TODO - project task 3
-          // Extend this method to satisfy requirements.
-          from withdraw amount
-          to deposit amount
-      }
-
+    def doTransaction() = {
       // TODO - project task 3
-      // make the code below thread safe
-      if (status == TransactionStatus.PENDING) {
-          doTransaction
-          Thread.sleep(50) // you might want this to make more room for
-                           // new transactions to be added to the queue
-      }
-
-
+      // Extend this method to satisfy requirements.
+      from withdraw amount
+      to deposit amount
     }
+
+    // TODO - project task 3
+    // make the code below thread safe
+    if (status == TransactionStatus.PENDING) {
+      doTransaction
+      Thread.sleep(50) // you might want this to make more room for
+      // new transactions to be added to the queue
+    }
+
+  }
 }
